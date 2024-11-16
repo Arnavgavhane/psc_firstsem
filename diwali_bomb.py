d83d3c7386f46edfb68b237ebdd218e3640bd575
Arnavgavhane <arnavgavhane1518@gmail.com>
2024-11-16 11:26:27 +0530

    Add files via upload

---
+++ diwali_bomb.py
@@ -0,0 +1,67 @@
+#print("happy Diwali")
+#n=int(input("enter a number"))
+#if n%4==0:
+ #  print('Yes')
+#elif n%5==0:
+# print('YES')
+#else:
+# print('NO')
+
+#for i in range(100):
+  #  print("Happy Diwali")
+#for i in range(1,101):
+ #   print(i)
+#for i in range(1,21):
+ #print(i**2)  
+#for i in range(1,21):
+#  a=i*i
+#  if a<200:
+#   print(a)
+#a=0
+#b=1
+#for i in range(10):
+# print(a)
+# c=a+b
+# a=b
+# b=c
+#n=int(input('enter a number'))
+#while i<n:
+# a=i+1
+# b=i*a 
+# i=i+1
+
+#print(b)
+#my_str='Hello_World'
+#print(my_str.split())
+#n=int(input("enter a number"))
+#sum = 0
+#for i in range(1,n+1):
+#    sum=sum+i
+#print(sum)
+#n=int(input("enter a number"))
+#sum=n*(n+1)/2
+#print(sum)
+#n=int(input("enter a number"))
+#sum_e=0
+#sum_o=0
+#i=0
+#for i in range(1,n+1):
+   # if i%2==0:
+  #      sum_e=sum_e+i    
+ #   else:
+#      sum_o=sum_o+i
+#print(sum_e)
+#print(sum_o)
+#n=int(input("enter a number"))
+#n_even= n//2
+#n_odd=n//2
+#if n%2==1:
+#    n_odd=n_odd+1
+#sum_even=n_even*(n_even+1)
+#sum_odd=n_odd*n_odd
+#print("even sum is", sum_even)
+#print("odd sum is", sum_odd)
+n=int(input("enter a number"))
+for i in range(1,n+1):
+    if n%i==0:
+        print(i)
